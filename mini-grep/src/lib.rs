d953209e669993308b6cff1a83edf065db43a030
Jose Dirazar <jose_f_dirazar@hotmail.com>
2024-06-08 05:29:44 -0300

    juici

--- mini-grep/src/lib.rs
+++ mini-grep/src/lib.rs
@@ -31,7 +31,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
     for line in contents.lines() {
         if line.contains(query) {
-            results.push(line);
+            results.push(line.trim());
         };
     }
         results
