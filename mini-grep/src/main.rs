e4eb763ce0906b86140d527552482c5051f068f9
Jo <jose_f_dirazar@hotmail.com>
2024-06-08 02:22:18 -0300

    package

--- mini-grep/src/main.rs
+++ mini-grep/src/main.rs
@@ -1,6 +1,5 @@
+use mini_grep::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,33 +11,8 @@ fn main() {
 
     println!("Searching for {} in file {}", config.query, config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = mini_grep::run(config) {
         println!("Application error: {}", e);
         process::exit(1)
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", content);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
