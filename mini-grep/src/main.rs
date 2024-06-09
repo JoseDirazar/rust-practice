45340003b01294fa9d3aa86f90a18e8aa5c333d6
Jose <JoseDirazar@github.com>
2024-06-09 20:03:20 -0300

    finish mini-grep´

--- mini-grep/src/main.rs
+++ mini-grep/src/main.rs
@@ -5,14 +5,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing argunments: {}", err);
+        eprintln!("Problem parsing argunments: {}", err);
         process::exit(1);
     });
 
     println!("Searching for {} in file {}", config.query, config.filename);
 
     if let Err(e) = mini_grep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1)
     }
 }
