d08f147cf69752abb0d23b8e0f232d72f829ad15
JoseDirazar <jose_f_dirazar@hotmail.com>
2024-06-14 23:28:43 -0300

    delete target folder

--- slice-type/target/debug/deps/slice_type-6eb92ddc9cc4459e.d
+++
@@ -1,5 +0,0 @@
-/home/fran/Desktop/rust-practice/slice-type/target/debug/deps/libslice_type-6eb92ddc9cc4459e.rmeta: src/main.rs
-
-/home/fran/Desktop/rust-practice/slice-type/target/debug/deps/slice_type-6eb92ddc9cc4459e.d: src/main.rs
-
-src/main.rs:
--- slice-type/target/debug/slice-type.d
+++
@@ -1,1 +0,0 @@
-/home/fran/Desktop/rust-practice/slice-type/target/debug/slice-type: /home/fran/Desktop/rust-practice/slice-type/src/main.rs
